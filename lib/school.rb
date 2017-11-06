fb6968176cefd55f1b7cc97f63f1117d522b8030
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-06 17:21:31 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,12 +3,12 @@ class School
 
   def initialize(name)
     @name = name
-    
+
   end
 
   def add_student(name, grade)
     @roster[grade] = [] unless @roster.include?(grade)
     @roster[grade] << name
   end
-  
+
 end
