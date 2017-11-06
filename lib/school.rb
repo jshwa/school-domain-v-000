359abeaa10cf93aeb10c23b9a1174656444a2335
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-06 17:32:33 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,17 @@
 class School
-  attr_accessor :roster
+  attr_accessor :name :roster
 
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-
   def add_student(name, grade)
     @roster[grade] = [] unless @roster.include?(grade)
     @roster[grade] << name
   end
 
+  def grade(g)
+    @roster[g]
+  end
 end
