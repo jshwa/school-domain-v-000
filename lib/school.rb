26f07836c4cc477b066bd2ebea4af59c4728c023
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-06 17:25:23 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,10 @@
 class School
+  attr_accessor :roster
 
-  def initialize(name, roster = {})
+  def initialize(name)
     @name = name
     @roster = {}
   end
-
 
 
   def add_student(name, grade)
